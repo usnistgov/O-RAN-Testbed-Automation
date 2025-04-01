802674a6ec04c44fcadfaf018185c91bf1ed798e
Wuthier, Simeon J. (Assoc) <simeon.wuthier@nist.gov>
2025-04-01 10:42:39 -0400

    Edit start_grafana_with_xapp_kpm_moni.sh

--- OpenAirInterface_Testbed/RAN_Intelligent_Controllers/Flexible-RIC/additional_scripts/start_grafana_with_xapp_kpm_moni.sh
+++ OpenAirInterface_Testbed/RAN_Intelligent_Controllers/Flexible-RIC/additional_scripts/start_grafana_with_xapp_kpm_moni.sh
@@ -65,8 +65,8 @@ if [ ! -f "$GRAFANA_LOG_FILE" ]; then
     echo "Creating Grafana log file at $GRAFANA_LOG_FILE..."
     sudo mkdir -p "$(dirname "$GRAFANA_LOG_FILE")"
     sudo touch "$GRAFANA_LOG_FILE"
-    sudo chown grafana:grafana "$GRAFANA_LOG_FILE"
-    sudo chmod g+r "$GRAFANA_LOG_FILE"
+    #sudo chown grafana:grafana "$GRAFANA_LOG_FILE"
+    #sudo chmod g+r "$GRAFANA_LOG_FILE"
 fi
 if ! command -v python3 &>/dev/null; then
     echo "Python3 not found, installing..."

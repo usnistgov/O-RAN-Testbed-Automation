e80f633e8c80cf6f97ae0f56a797435d642bf616
Simewu <simeon.wuthier@nist.gov>
2026-05-19 10:37:37 -0400

    Update commit hashes to latest stable versions
    OAI w20 is broken

--- OpenAirInterface_Testbed/RAN_Intelligent_Controllers/Flexible-RIC/full_install.sh
+++ OpenAirInterface_Testbed/RAN_Intelligent_Controllers/Flexible-RIC/full_install.sh
@@ -37,8 +37,8 @@ FLEXRIC_LIBRARY_DIR="flexric/build/flexric_libraries/lib/flexric/"
 APPLY_PATCHES=true
 CLEAN_INSTALL=false
 DEBUG_SYMBOLS=false
-E2AP_VERSION="E2AP_V2"        # E2AP_V1, E2AP_V2, E2AP_V3
-KPM_VERSION="KPM_V2_03"       # KPM_V2_03, KPM_V3_00
+E2AP_VERSION="E2AP_V3"        # E2AP_V1, E2AP_V2, E2AP_V3
+KPM_VERSION="KPM_V3_00"       # KPM_V2_03, KPM_V3_00
 E2_TERM_PORT=36421            # Ensure this matches the gNodeB's full_install.sh E2_TERM_PORT. Default is 36421, which will result in no modification
 E2_TERM_PORT_SUBSTITUTE=36423 # If E2_TERM_PORT is used already, substitute it before replacing with E2_TERM_PORT
 APTVARS="NEEDRESTART_MODE=l NEEDRESTART_SUSPEND=1 DEBIAN_FRONTEND=noninteractive"

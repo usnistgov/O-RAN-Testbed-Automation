c728f8788c5ad48a4fe7844c0496268abb39a25f
Simewu <simeon.wuthier@nist.gov>
2026-06-29 00:16:33 -0400

    Cleaner error when failing to install GCC-13

--- User_Equipment/full_install.sh
+++ User_Equipment/full_install.sh
@@ -134,12 +134,10 @@ else
 fi
 if [[ "$INSTALL_GCC" == "true" ]]; then
     echo "Installing GCC 13..."
-    UBUNTU_CODENAME=$(./install_scripts/get_ubuntu_codename.sh)
-    if ! curl -fsSL --connect-timeout 10 "https://ppa.launchpadcontent.net/ubuntu-toolchain-r/test/ubuntu/dists/${UBUNTU_CODENAME}/Release" >/dev/null; then
-        echo "ERROR: Cannot reach the Ubuntu Toolchain PPA package repository."
+    if ! sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test; then
+        echo "ERROR: Failed to add the Ubuntu Toolchain PPA."
         exit 1
     fi
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
     sudo apt-get update
     sudo env $APTVARS apt-get install -y gcc-13 g++-13
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 100

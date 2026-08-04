2c177a9481163f1fc6944c6ca7fb66cd08c23158
Simewu <simeon.wuthier@nist.gov>
2026-08-04 15:58:29 -0400

    Fix docker version typo preventing installation on Ubuntu 26.04

--- OpenAirInterface_Testbed/Next_Generation_Node_B/install_scripts/install_docker.sh
+++ OpenAirInterface_Testbed/Next_Generation_Node_B/install_scripts/install_docker.sh
@@ -96,7 +96,6 @@ else # Use docker-ce
         DOCKERV="28.1"
     fi
 
-    DOCKERV="28.1"
     UBUNTU_CODENAME=$(grep -oP '^UBUNTU_CODENAME=\K.*' /etc/os-release 2>/dev/null)
     # If not found, try to extract VERSION_CODENAME as a fallback
     if [[ -z "$UBUNTU_CODENAME" ]]; then

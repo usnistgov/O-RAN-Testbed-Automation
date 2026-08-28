fed45aeb51130edb2c0e497de29802a6007cdc63
Simewu <simeon.wuthier@nist.gov>
2026-08-28 12:50:10 -0400

    Install k9s earlier (so users can see the pods as they're being deployed)

--- RAN_Intelligent_Controllers/Near-Real-Time-RIC/install_scripts/install_k9s.sh
+++ RAN_Intelligent_Controllers/Near-Real-Time-RIC/install_scripts/install_k9s.sh
@@ -34,56 +34,55 @@ echo "# Script: $(realpath "$0") $@"
 set -e
 
 # Check if k9s is already installed
-if command -v k9s &>/dev/null; then
-    echo "Already installed k9s, skipping."
-    exit 0
-fi
+if ! command -v k9s &>/dev/null; then
+    echo "Downloading and extracting k9s..."
 
-echo "Downloading and extracting k9s..."
+    # Create and navigate to the installation directory
+    mkdir -p "$HOME/k9s-installation"
+    cd "$HOME/k9s-installation"
 
-# Create and navigate to the installation directory
-mkdir -p "$HOME/k9s-installation"
-cd "$HOME/k9s-installation"
+    # Determine the processor architecture
+    ARCH_SUFFIX=""
+    case $(uname -m) in
+    "x86_64")
+        ARCH_SUFFIX="Linux_amd64"
+        ;;
+    "aarch64")
+        ARCH_SUFFIX="Linux_arm64"
+        ;;
+    "armv7l")
+        ARCH_SUFFIX="Linux_armv7"
+        ;;
+    "ppc64le")
+        ARCH_SUFFIX="Linux_ppc64le"
+        ;;
+    "s390x")
+        ARCH_SUFFIX="Linux_s390x"
+        ;;
+    *)
+        echo "Unsupported architecture: $(uname -m)"
+        exit 1
+        ;;
+    esac
 
-# Determine the processor architecture
-ARCH_SUFFIX=""
-case $(uname -m) in
-"x86_64")
-    ARCH_SUFFIX="Linux_amd64"
-    ;;
-"aarch64")
-    ARCH_SUFFIX="Linux_arm64"
-    ;;
-"armv7l")
-    ARCH_SUFFIX="Linux_armv7"
-    ;;
-"ppc64le")
-    ARCH_SUFFIX="Linux_ppc64le"
-    ;;
-"s390x")
-    ARCH_SUFFIX="Linux_s390x"
-    ;;
-*)
-    echo "Unsupported architecture: $(uname -m)"
-    exit 1
-    ;;
-esac
+    # Construct the download URL using the determined architecture suffix
+    DOWNLOAD_URL="https://github.com/derailed/k9s/releases/latest/download/k9s_${ARCH_SUFFIX}.tar.gz"
 
-# Construct the download URL using the determined architecture suffix
-DOWNLOAD_URL="https://github.com/derailed/k9s/releases/latest/download/k9s_${ARCH_SUFFIX}.tar.gz"
-
-# Download and check if the curl command was successful
-if curl -fLO "$DOWNLOAD_URL"; then
-    tar -xzf k9s_${ARCH_SUFFIX}.tar.gz
-    sudo install -m 755 k9s /usr/local/bin/k9s
-    rm k9s_${ARCH_SUFFIX}.tar.gz
-    mkdir -p "$HOME/.config/k9s"
-    echo "Successfully installed k9s."
+    # Download and check if the curl command was successful
+    if curl -fLO "$DOWNLOAD_URL"; then
+        tar -xzf "k9s_${ARCH_SUFFIX}.tar.gz"
+        sudo install -m 755 k9s /usr/local/bin/k9s
+        rm "k9s_${ARCH_SUFFIX}.tar.gz"
+        mkdir -p "$HOME/.config/k9s"
+        echo "Successfully installed k9s."
+    else
+        echo "Failed to download k9s for the architecture: ${ARCH_SUFFIX}"
+        # Clean up the installation directory if the download fails
+        cd ..
+        rm -rf "$HOME/k9s-installation"
+    fi
 else
-    echo "Failed to download k9s for the architecture: ${ARCH_SUFFIX}"
-    # Clean up the installation directory if the download fails
-    cd ..
-    rm -rf "$HOME/k9s-installation"
+    echo "Already installed k9s, skipping."
 fi
 
 if ! command -v kubecolor &>/dev/null; then
